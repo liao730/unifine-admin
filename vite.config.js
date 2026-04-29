46b18610a8668b0ca7ba63630a72e84cb161fbaa
廖威 <@liaowei>
2026-04-29 17:15:35 +0800

    prod修改

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig(({ command }) => {
     proxy: {
       // 将所有以 /api 开头的请求转发到后端
       '/altra/api': {
-        target: 'http://localhost:8080',
+        target: 'http://backend:8080',
+        // target: 'http://localhost:8080',
         changeOrigin: true,
         // 如果后端接口路径里没有 /api，可以开启路径重写
         // rewrite: (path) => path.replace(/^\/api/, '') 
